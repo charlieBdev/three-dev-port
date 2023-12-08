7254aab6561b4e3f8609ffa03fcc20de8018f677
charlieBdev <chazbish@hotmail.com>
2023-12-08 12:34:51 +0000

    finish About

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,53 +3,70 @@ import { Canvas } from '@react-three/fiber';
 import React, { Suspense, useEffect, useState } from 'react';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
-	const computer = useGLTF('./desktop_pc/scene.gltf');
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF('./planet/scene.gltf');
 
 	return (
 		<mesh>
 			<hemisphereLight intensity={0.15} groundColor='black' />
-			<pointLight intensity={1} />
 			<spotLight
 				position={[-20, 50, 10]}
-				angle={[0.12]}
+				angle={0.12}
 				penumbra={1}
 				intensity={1}
 				castShadow
 				shadow-mapSize={1024}
 			/>
+			<pointLight intensity={1} />
 			<primitive
 				object={computer.scene}
-				scale={0.75}
-				position={[0, -3.25, -1.5]}
+				scale={isMobile ? 1.1 : 1.75}
+				// scale={[4, 4, 4]}
+				position={isMobile ? [0, -1, 0] : [0, -1, 0]}
 				rotation={[-0.01, -0.2, -0.1]}
 			/>
 		</mesh>
 	);
 };
 
-const ComputerCanvas = () => {
+const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 500px)');
+		setIsMobile(mediaQuery.matches);
+
+		const handleMediaQueryChange = (e) => {
+			setIsMobile(e.matches);
+		};
+
+		mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+		return () => {
+			mediaQuery.removeEventListener('change', handleMediaQueryChange);
+		};
+	}, []);
+
 	return (
 		<Canvas
-			frameLoop='demand'
+			frameloop='demand'
 			shadows
-			// field of view
-			camera={{ position: [20, 3, 5], fov: 25 }}
+			camera={{ position: [20, 3, 5], fov: 20 }}
 			gl={{ preserveDrawingBuffer: true }}
 		>
 			// Suspense is from React - allows CanvasLoader while loading
 			<Suspense fallback={<CanvasLoader />}>
-				// min and maxPolarAngle prevent 360 rotation
+				// min and maxPolarAngle prevent 360 rotation?
 				<OrbitControls
 					enableZoom={false}
 					maxPolarAngle={Math.PI / 2}
 					minPolarAngle={Math.PI / 2}
 				/>
-				<Computers />
+				<Computers isMobile={isMobile} />
 			</Suspense>
 			<Preload all />
 		</Canvas>
 	);
 };
 
-export default Computers;
+export default ComputersCanvas;
